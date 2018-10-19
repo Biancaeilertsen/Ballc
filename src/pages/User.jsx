ca37124ae6d015082e22eff1445ee2a969c02909
Biancaeilertsen <biancaeilertsen@gmail.com>
2018-10-19 11:41:03 +0200

    j

---
+++ src/pages/User.jsx
@@ -0,0 +1,38 @@
+import React,  { Component } from 'react';
+import Form from '../components/Form';
+import Personlist from '../components/Personlist';
+import PersonInput from '../components/PersonInput';
+
+export default class User extends Component {
+/*  state = {
+      fields: {}
+    };
+
+    onChange = updatedValue => {
+      this.setState({
+        fields: {
+          ...this.state.fields,
+          ...updatedValue
+        }
+      });
+    };
+
+    render() {
+      return (
+        <div className="App">
+          <Form onChange={fields => this.onChange(fields)} />
+          <p>
+            {JSON.stringify(this.state.fields, null, 2)}
+          </p>
+        </div>
+      );
+    }*/
+    render(){
+      return(
+        <div>
+          <PersonInput />
+          <Personlist/>
+        </div>
+      )
+    }
+  }
